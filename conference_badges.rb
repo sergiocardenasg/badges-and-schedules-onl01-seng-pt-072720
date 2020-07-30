7bf86c36e5b7fdc55a7d73f8d3777e77687830bb
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-07-30 00:23:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker (arr)
+  arr = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  counter = 0
+  arr.each do |name|
+    while counter < arr.length-1
+      puts "Hello, my name is #{name}"
+    end
+  end
+end
